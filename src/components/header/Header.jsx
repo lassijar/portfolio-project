ba7efc2de8b9dd29f92fdb0abbd3fa97972925e5
lassi <lassi.jarvela@gmail.com>
2023-07-02 10:18:59 +0300

    configured headers component and added two buttons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./header.css";
+import CTA from "./CTA";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello! My name is</h5>
+        <h1>Lassi Järvelä</h1>
+        <h5 className="text-light"> Computer science student</h5>
+        <CTA />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
